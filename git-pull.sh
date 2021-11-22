40d46a76aab90cdcc70f2bdf85edcf6d4d0ed932
liuconglook <68932797+liuconglook@users.noreply.github.com>
2021-11-22 14:52:09 +0800

    Update git-pull.sh

--- git-pull.sh
+++ git-pull.sh
@@ -15,9 +15,12 @@ echo "DESTINATION=$DESTINATION_REPO"
 git clone "$DESTINATION_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
 # 添加源仓库
 git remote add dest "$SOURCE_REPO"
+mv index.html index
 # 更新代码
 git pull dest master
 # 忽略index文件
-git rm --cached index.html
+mv index index.html
+git add index.html
+git commit index.html -m'update'
 # 提交更新
 git push origin master
